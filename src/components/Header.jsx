e1f02bf33000d02de3e991fa993057158d111081
Victor Torres <vatorresm88@gmail.com>
2023-08-29 17:24:40 -0500

    Terminando el Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,24 @@ import React from 'react';
 import CenterMenu from './CenterMenu';
 
 function Header() {
-  const buttonsStyle =
+  const buttonStyle =
     'border-[2px] rounded-[10px] border-[#232A4E] px-[25px] py-[7px]';
   return (
     <div className='header bg-[#081730] flex items-center justify-between px-[5rem] pt-[2.4rem] text-[0.8rem]'>
+      {/* logo */}
       <img
         src={require('../img/MuzicLogo.png')}
-        className='logo w-[42px] h-[42px]'
-        alt='Logo'
+        alt=''
+        className='logo  w-[42px] h-[42px]'
       />
+      {/* side menu */}
       <CenterMenu />
+      {/* buttons */}
       <div className='buttons flex'>
-        <button className={`mr-[35px] hover:bg-[#232A4E]` + buttonsStyle}>
-          Sign up
+        <button className={`mr-[35px] hover:bg-[#232A4E] ` + buttonStyle}>
+          Log in
         </button>
-        <button className={buttonsStyle + `bg-[#232A4E]`}>Log In</button>
+        <button className={buttonStyle + ` bg-[#232A4E]`}>Sign up</button>
       </div>
     </div>
   );
