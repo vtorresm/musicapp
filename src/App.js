e523dbb524b29289d50bbd708c5355c911474afa
Victor Torres <vatorresm88@gmail.com>
2023-08-29 17:35:56 -0500

    Agregando componentes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-
+import Download from './components/Download';
+import Experience from './components/Experience';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Experience from './components/Experience';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className='App text-white overflow-hidden'>
-      <Header />
-      <Hero />
-      <Experience />
+    <div className="App text-white overflow-hidden">
+        <Header/>
+        <Hero/>
+        <Experience/>
+        <Search/>
+        <Download/>
+        <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
